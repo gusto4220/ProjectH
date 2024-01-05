61370ab617eea440cecb7c77c4ffa2caeb2f221d
gustavocruz <gustavocruz@gustavos-air.attlocal.net>
2024-01-04 23:06:51 -0600

    Push Attempt 3

--- src/DemoH.java
+++ src/DemoH.java
@@ -7,6 +7,12 @@ public class DemoH {
 
 		//Pull Attempt 3
 		System.out.println("Homework Done!");
+		
+		//Push Attempt 3
+		String day = "Thursday";
+		String month = "January";
+		int dayNum = 4;
+		System.out.println("Today's Date: " + day + ", " + month + " " + dayNum + "th.");
 
 	}
 
